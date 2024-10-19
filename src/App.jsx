5b963445540e9aa638cc72f9bbd43c204368276a
HuynhKimhuy <huynhkimhuy69@gmail.com>
2024-10-19 14:55:14 +0700

    test colaborators

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
+          This is a little changes
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
